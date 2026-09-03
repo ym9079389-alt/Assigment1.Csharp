1af22aa0dd30b57228259f0793703a9669037481
ym9079389-alt <ym9079389@gmail.com>
2026-09-03 22:26:22 +0300

    Add project files.

---
+++ Books.cs
@@ -0,0 +1,19 @@
+﻿namespace AssignmentCSharp
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Pages { get; set; }
+
+        public Book(string title, int pages)
+        {
+            Title = title;
+            Pages = pages;
+        }
+
+        public override string ToString()
+        {
+            return $"Book Title: {Title}, Pages: {Pages}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace AssignmentCSharp
+{
+        internal class Program
+        {
+            static void Main(string[] args)
+            {
+                #region 1
+                Book myBook = new Book("C# Programming", 300);
+                object bookObj = myBook;
+                Console.WriteLine(bookObj);
+                #endregion
+
+                
+            }
+        }
+}
