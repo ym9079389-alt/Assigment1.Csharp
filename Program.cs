18c554cadac318953f8075d991280d3fe84e92d7
ym9079389-alt <ym9079389@gmail.com>
2026-09-03 22:35:06 +0300

    Assigment 1

--- Program.cs
+++ Program.cs
@@ -4,7 +4,111 @@
         {
             static void Main(string[] args)
             {
-               
+            #region 1
+            Book myBook = new Book("C# Programming", 300);
+            object bookObj = myBook;
+            Console.WriteLine(bookObj);
+            #endregion
+
+            Console.WriteLine($"=======\n");
+
+            #region 2
+            Console.WriteLine(myBook.ToString());
+            Console.WriteLine(myBook.Equals(myBook));
+            Console.WriteLine(myBook.GetHashCode());
+            Console.WriteLine(myBook.GetType());
+            #endregion
+
+            Console.WriteLine($"=======\n");
+
+            #region 3
+            int pages = 464;
+            Console.WriteLine(pages);
+            #endregion
+
+            Console.WriteLine($"=======\n");
+
+            #region 4
+            try
+            {
+                int n = 10;
+                int x = 0;
+                int res = n / x;
+                Console.WriteLine(res);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("Done");
+            }
+            #endregion
+
+            Console.WriteLine($"=======\n");
+
+            #region 5
+            int intPages = 300;
+            double doublePages = intPages;
+            Console.WriteLine(doublePages);
+            #endregion
+
+            Console.WriteLine($"=======\n");
+
+            #region 6
+            double dprice = 49.99;
+            int iprice = (int)dprice;
+            Console.WriteLine(iprice);
+            #endregion
+
+            Console.WriteLine($"=======\n");
+
+            #region 7
+            string pagesText = "464"; //[cite: 1]
+            int convertedPages = Convert.ToInt32(pagesText);
+            Console.WriteLine(convertedPages);
+            #endregion
+
+            Console.WriteLine($"=======\n");
+
+            #region 8
+            string yearText = "2023";
+            int year = int.Parse(yearText);
+            Console.WriteLine(year);
+
+
+
+            string badText = "abc";
+            if (int.TryParse(badText, out int resu))
+            {
+                Console.WriteLine("Invalid number");
+            }
+            #endregion
+
+            Console.WriteLine($"=======\n");
+
+            #region 9
+            int pagesNum = 464;
+            string strPages = pagesNum.ToString();
+            Console.WriteLine(strPages, strPages.GetType());
+            #endregion
+
+            Console.WriteLine($"=======\n");
+
+            #region 10
+            int copies = 100;
+            object boxedCopies = copies;
+            int unboxedCopies = (int)boxedCopies;
+
+            Console.WriteLine(boxedCopies);
+            Console.WriteLine(unboxedCopies);
+            #endregion
+
         }
     }
 }
