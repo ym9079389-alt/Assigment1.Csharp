12698c0a4701c43ddfb0975b9aa01b990428bebf
ym9079389-alt <ym9079389@gmail.com>
2026-09-03 22:37:05 +0300

    Q 4

--- Program.cs
+++ Program.cs
@@ -28,6 +28,28 @@
 
             Console.WriteLine($"=======\n");
 
+            #region 4
+            try
+            {
+                int n = 10;
+                int x = 0;
+                int res = n / x;
+                Console.WriteLine(res);
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace);
+            }
+            finally
+            {
+                Console.WriteLine("Done");
+            }
+            #endregion
+
             
 
         }
