4e181c4174c27b0ac9d8323c5204fe13d6e896e3
ym9079389-alt <ym9079389@gmail.com>
2026-09-03 22:38:14 +0300

    Q 7

--- Program.cs
+++ Program.cs
@@ -66,7 +66,15 @@
             Console.WriteLine(iprice);
             #endregion
 
-           
+            Console.WriteLine($"=======\n");
+
+            #region 7
+            string pagesText = "464"; //[cite: 1]
+            int convertedPages = Convert.ToInt32(pagesText);
+            Console.WriteLine(convertedPages);
+            #endregion
+
+            
 
         }
     }
