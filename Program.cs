7a559ee0fcf33c3449a897efa8f368b5458f1ff1
ym9079389-alt <ym9079389@gmail.com>
2026-09-03 22:28:19 +0300

    Q 3

--- Program.cs
+++ Program.cs
@@ -19,6 +19,13 @@
             Console.WriteLine(myBook.GetType());
             #endregion
 
+            Console.WriteLine($"=======\n");
+
+            #region 3
+            int pages = 464;
+            Console.WriteLine(pages);
+            #endregion
+
             
 
         }
